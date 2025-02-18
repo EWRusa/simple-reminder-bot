1282f5a9075201371ead719330db082314957f72
EWRusa <stringcheeseinc1@gmail.com>
2025-02-18 15:03:06 -0700

    Added discord commands for how I plan for reminders to be used.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,8 @@
 import configparser
 import discord
 
+my_intents = discord.Intents.default()
+my_intents.message_content = True
+bot_version = '0.1'
 config = configparser.ConfigParser()
 config.read('../resources/application.properties')
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from __init__ import *
+import discord_commands
 
-if __name__ == "__main__":
-    print('Hello World')
-    print(config.get('DiscordSection', 'token'))
+if __name__ == '__main__':
+    discord_commands.run(config.get('DiscordSection','token'))
